de25b395cde77025f6973d4958c70420af5a84d6
Ivan Jovic <ivan.jovic@camunda.com>
2022-11-29 10:19:41 +0100

    initial commit

---
+++ src/main/java/com/camunda/demo/CamundaSpringBootLearningApplication.java
@@ -0,0 +1,13 @@
+package com.camunda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaSpringBootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaSpringBootLearningApplication.class, args);
+    }
+
+}
